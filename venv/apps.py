12898e36fcbb1f4537f0dda2fa686d40599a2d4c
Humayra Musarrat Tayeaba <159180272+HumayraTayba@users.noreply.github.com>
2025-05-12 13:24:13 +0600

    Add files via upload

--- venv/apps.py
+++ venv/apps.py
@@ -1,8 +1,17 @@
 from django.apps import AppConfig
+from django.contrib.sites.checks import check_site_id
+from django.core import checks
+from django.db.models.signals import post_migrate
 from django.utils.translation import gettext_lazy as _
 
+from .management import create_default_site
 
-class SiteMapsConfig(AppConfig):
+
+class SitesConfig(AppConfig):
     default_auto_field = "django.db.models.AutoField"
-    name = "django.contrib.sitemaps"
-    verbose_name = _("Site Maps")
+    name = "django.contrib.sites"
+    verbose_name = _("Sites")
+
+    def ready(self):
+        post_migrate.connect(create_default_site, sender=self)
+        checks.register(check_site_id, checks.Tags.sites)
