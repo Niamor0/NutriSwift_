17320be68c3ac0bdb79889cf1649b82b33f8eb99
Humayra Musarrat Tayeaba <159180272+HumayraTayba@users.noreply.github.com>
2025-05-12 13:23:49 +0600

    Add files via upload

--- venv/apps.py
+++ venv/apps.py
@@ -2,6 +2,7 @@ from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class SessionsConfig(AppConfig):
-    name = "django.contrib.sessions"
-    verbose_name = _("Sessions")
+class SiteMapsConfig(AppConfig):
+    default_auto_field = "django.db.models.AutoField"
+    name = "django.contrib.sitemaps"
+    verbose_name = _("Site Maps")
